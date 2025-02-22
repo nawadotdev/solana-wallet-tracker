5c3a4a7c73a99295edc57ccb5d9639444d5791df
devnawa <148815756+devnawa@users.noreply.github.com>
2025-02-22 12:03:39 +0300

    v0.4.3
    finish tracking for fetch required programs

--- src/services/Solana.service.ts
+++ src/services/Solana.service.ts
@@ -11,3 +11,32 @@ export const unsubscribeLogs = (subscriptionId : number) => {
     Connection.removeOnLogsListener(subscriptionId)
     return true
 }
+
+const MAX_RETRIES = 10;
+const RETRY_DELAY_MS = 1000;
+
+export const fetchTransaction = async (signature: string) => {
+    let attempts = 0;
+  
+    while (attempts < MAX_RETRIES) {
+      try {
+        const tx = await Connection.getParsedTransaction(signature, {
+          maxSupportedTransactionVersion: 0,
+          commitment: "confirmed",
+        });
+  
+        if (tx) {
+          return tx;
+        }
+  
+        await delay(RETRY_DELAY_MS * attempts);
+      } catch (error) {
+        console.error("Transaction fetch error:", error);
+      }
+      attempts++;
+    }
+    return null;
+  };
+  
+  const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+  
--- src/utils/logsCallback.ts
+++ src/utils/logsCallback.ts
@@ -1,9 +1,10 @@
-import { Logs, ParsedTransactionWithMeta } from "@solana/web3.js";
+import { Logs, ParsedTransactionWithMeta, PartiallyDecodedInstruction } from "@solana/web3.js";
 import { TradeData } from "../types/trade";
 import { logParser } from "./logParser";
 import { programIdMap } from "../lib/Programs";
+import { fetchTransaction } from "../services/Solana.service";
 
-export const logsCallback = (cb: Logs) => {
+export const logsCallback = async (cb: Logs) => {
 
     if (cb.err) return
 
@@ -12,42 +13,56 @@ export const logsCallback = (cb: Logs) => {
 
     if (signature === "1111111111111111111111111111111111111111111111111111111111111111") return
 
-    try{
+    try {
 
-        let tx : ParsedTransactionWithMeta | null = null
-        let trades : TradeData[] = []
+        let tx: ParsedTransactionWithMeta | null = null
+        let trades: TradeData[] = []
 
         const grouppedLogs = logParser(logs)
 
-        for(let i = 0; i<grouppedLogs.length; i++){
+        for (let i = 0; i < grouppedLogs.length; i++) {
             const grouppedLog = grouppedLogs[i]
             const programId = grouppedLog.programId?.toString()
 
-            if(!programId) continue
+            if (!programId) continue
 
             const program = programIdMap[programId]
 
-            if(program && !program.tradeProgram) continue
+            if (program && !program.tradeProgram) continue
 
-            let tradeData : TradeData | null = null
+            let tradeData: TradeData | null = null
 
-            if(program.fetchRequired){
-            }else{
+            if (program.fetchRequired) {
+                tx = await fetchTransaction(signature)
+
+                if (!tx) throw new Error("Transaction not found")
+
+                const instruction = tx.transaction.message.instructions[i] || null
+
+                if (!instruction) throw new Error("Instruction not found")
+
+                tradeData = program.getTradeData({
+                    transaction: tx,
+                    instruction: instruction as PartiallyDecodedInstruction,
+                    index: i,
+                    outerIndex: null
+                })
+
+                if (tradeData) trades.push(tradeData)
+            } else {
                 tradeData = program.getTradeData({
                     logs: grouppedLog.logs,
                 })
             }
 
-            if(tradeData){
-                trades.push(tradeData)
-            }
+            if (tradeData) trades.push(tradeData)
         }
 
-        if(trades.length > 0){
+        if (trades.length > 0) {
             console.log(trades)
         }
 
-    }catch(err){
+    } catch (err) {
         console.error(err)
     }
 
